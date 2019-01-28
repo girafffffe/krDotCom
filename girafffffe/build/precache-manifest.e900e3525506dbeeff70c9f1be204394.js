cc606b50c4245a7efe2492b655bb9ba0822e2c51
Ki Rodriguez <krodrig@kirodriguez.localdomain>
2019-01-28 15:46:16 -0500

    adding build

---
+++ girafffffe/build/precache-manifest.e900e3525506dbeeff70c9f1be204394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "614e374c9b19ebc8988c",
+    "url": "/static/js/main.614e374c.chunk.js"
+  },
+  {
+    "revision": "930d226cfc2db032e2e8",
+    "url": "/static/js/1.930d226c.chunk.js"
+  },
+  {
+    "revision": "614e374c9b19ebc8988c",
+    "url": "/static/css/main.09248a76.chunk.css"
+  },
+  {
+    "revision": "b2c3d8c20c4b3f92221b9a3daf413214",
+    "url": "/index.html"
+  }
+];
