bddfc5be3b0c1f5787f1c94d94abbf40df6a8b7e
girafffffe <44880619+girafffffe@users.noreply.github.com>
2019-04-17 15:56:57 -0400

    Update Lockup.js

--- girafffffe/src/Components/Lockup.js
+++ girafffffe/src/Components/Lockup.js
@@ -8,7 +8,6 @@ class Lockup extends React.Component {
       <div className="content-wrap">
 	      <div className="content-wrap__lockup">
 		      <h1 className="fas fa-file-medical content-wrap__title--main animated wave"></h1>
-		        <h1 className="content-wrap__title--main">1.25.2019</h1>
 		        <p className="content-wrap__title--secondary">updating architecture</p>
 	      </div>
       </div>
