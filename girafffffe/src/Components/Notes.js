c957931c2965b8611a439872f28836a42dcd1f27
girafffffe <44880619+girafffffe@users.noreply.github.com>
2019-04-17 15:58:48 -0400

    Update Notes.js

--- girafffffe/src/Components/Notes.js
+++ girafffffe/src/Components/Notes.js
@@ -7,7 +7,7 @@ class Notes extends React.Component {
     return (
       {
         <div>
-     
+     <!--Currently making my way through the React transition for this site. STAY TUNED-->
     </div>
     }
         
